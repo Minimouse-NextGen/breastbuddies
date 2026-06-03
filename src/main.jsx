dabb3b15af2a83c321593c295fde8163395a2f8a
Minimouse-NextGen <Minimouse-NextGen@users.noreply.github.com>
2026-06-04 01:20:03 +0530

    Add Vercel Analytics and Speed Insights_1

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
@@ -8,8 +9,10 @@ import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-    <Analytics />
-    <SpeedInsights />
+    <BrowserRouter>
+      <App />
+      <Analytics />
+      <SpeedInsights />
+    </BrowserRouter>
   </React.StrictMode>
 );
